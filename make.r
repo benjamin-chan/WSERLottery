514579c2ac21d8a15bbcd2ac4eb0f73efa17a135
Benjamin Chan <chanb@ohsu.edu>
2014-11-17 16:37:21 -0800

    Test parallelization

--- make.r
+++ make.r
@@ -1,3 +1,9 @@
 require(rmarkdown)
-render(file.path(getwd(), "WSERLottery.Rmd"), output_format="ioslides_presentation")
-file.rename(file.path(getwd(), "WSERLottery.html"), file.path(getwd(), "index.html"))
+path <- "Users\\chanb\\Documents\\GitHub Repositories\\WSERLottery\\"
+if (Sys.info()["nodename"] == "CHSE") {
+  path <- paste0("\\\\tsclient\\C\\", path)
+} else {
+  path <- paste0("C:\\", path)
+}
+render(paste0(path, "WSERLottery.Rmd"), output_format="ioslides_presentation")
+file.rename(paste0(path, "WSERLottery.html"), paste0(path, "index.html"))
