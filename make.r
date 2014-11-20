9985e37f29edc2bbf301e5d10106cb4e8e7c7789
Benjamin Chan <benjamin.ks.chan@gmail.com>
2014-11-19 23:45:45 -0800

    Update with new tickets counts
    New ticket counts beginning with 2015 race lottery:
    
    Each runner who enters the lottery and fails to gain entry into the Run (and
    otherwise doesn’t gain an entry via other means such as an aid station,
    sponsor, or MUC spot) will have additional tickets in the hat when entering
    the lottery the following year, thus improving the probability of being
    selected. Every lottery applicant will receive 2^(n-1) tickets in the hat for
    each consecutive failure to gain entry where n is the number of years applied.
    As an example: If a runner applies and fails to gain entry two consecutive
    times, they will have four tickets in the hat when entered in the lottery the
    third consecutive year (2^2), eight in fourth consecutive year (2^3), 16 in
    fifth (2^4), 32 in sixth (2^5), etc.  Conversely, if a runner enters the
    lottery one year, is not chosen and does not enter the following year, they
    will not have a extra tickets in the hat when applying the year after that.

--- make.r
+++ make.r
@@ -2,8 +2,10 @@ require(rmarkdown)
 path <- "Users\\chanb\\Documents\\GitHub Repositories\\WSERLottery\\"
 if (Sys.info()["nodename"] == "CHSE") {
   path <- paste0("\\\\tsclient\\C\\", path)
-} else {
+} else if (Sys.info()["nodename"] == "CHANB") {
   path <- paste0("C:\\", path)
+} else if (Sys.info()["nodename"] == "FAMILYPC") {
+  path <- "C:\\Users\\Ben\\Documents\\GitHub repositories\\WSERLottery\\"
 }
 render(paste0(path, "WSERLottery.Rmd"), output_format="ioslides_presentation")
 file.rename(paste0(path, "WSERLottery.html"), paste0(path, "index.html"))
