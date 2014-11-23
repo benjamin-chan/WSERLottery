f67984239808400e50031e6c08d3c39c2c3824ff
Benjamin Chan <benjamin.ks.chan@gmail.com>
2014-11-23 18:58:08 -0500

    Update 2015 lottery with 2015 ticket distribution

--- make.r
+++ make.r
@@ -6,6 +6,8 @@ if (Sys.info()["nodename"] == "CHSE") {
   path <- paste0("C:\\", path)
 } else if (Sys.info()["nodename"] == "FAMILYPC") {
   path <- "C:\\Users\\Ben\\Documents\\GitHub repositories\\WSERLottery\\"
+} else if (Sys.info()["nodename"] == "MAMAYAGA") {
+  path <- "C:\\Users\\Cat Buckley\\Documents\\GitHub\\WSERLottery\\"
 }
 render(paste0(path, "WSERLottery.Rmd"), output_format="ioslides_presentation")
 file.rename(paste0(path, "WSERLottery.html"), paste0(path, "index.html"))
