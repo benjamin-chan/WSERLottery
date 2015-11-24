80ffa05388380b2dac36602420b28ac09070c2af
Benjamin Chan <chanb@ohsu.edu>
2015-11-23 16:23:20 -0800

    Update 2016 lottery with 2016 ticket distribution

--- make.r
+++ make.r
@@ -4,18 +4,8 @@ if (!require(data.table)) {install.packages("data.table", dependencies=TRUE)}
 if (!require(reshape2)) {install.packages("reshape2", dependencies=TRUE)}
 if (!require(ggplot2)) {install.packages("ggplot2", dependencies=TRUE)}
 if (!require(xtable)) {install.packages("xtable", dependencies=TRUE)}
+if (!require(knitr)) {install.packages("knitr", dependencies=TRUE)}
 if (!require(rmarkdown)) {install.packages("rmarkdown", dependencies=TRUE)}
 require(rmarkdown)
-if (Sys.info()["nodename"] == "CHSE") {
-  path <- paste0("\\\\tsclient\\C\\", path)
-} else if (Sys.info()["nodename"] == "GHBA299") {
-  path <- paste0("C:\\", path)
-} else if (Sys.info()["nodename"] == "FAMILYPC") {
-  path <- "C:\\Users\\Ben\\Documents\\GitHub repositories\\WSERLottery\\"
-} else if (Sys.info()["nodename"] == "MAMAYAGA") {
-  path <- "C:\\Users\\Cat Buckley\\Documents\\GitHub\\WSERLottery\\"
-} else {
-  path <- "Dropbox/"
-}
-render(paste0(path, "WSERLottery.Rmd"), output_format="ioslides_presentation")
-file.rename(paste0(path, "WSERLottery.html"), paste0(path, "index.html"))
+render(paste(getwd(), "WSERLottery.Rmd", sep="/"), output_format="ioslides_presentation")
+file.rename(paste(getwd(), "WSERLottery.html", sep="/"), paste(getwd(), "index.html", sep="/"))
