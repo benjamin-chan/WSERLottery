0afaf7ee29bc3bea6a7195b99da24806a806f5ff
Benjamin Chan <benjamin.ks.chan@gmail.com>
2017-11-16 22:03:39 -0800

    Update the checkpoint snapshot

--- make.r
+++ make.r
@@ -1,5 +1,6 @@
+setwd("~/R/Projects/WSERLottery")
 library(checkpoint)
-checkpoint("2016-10-01", use.knitr = TRUE)
+checkpoint("2017-11-01", use.knitr = TRUE)
 library(knitr)
 library(rmarkdown)
 knit("WSERLottery.Rmd", output = "README.md")
