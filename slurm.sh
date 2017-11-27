d4e33291e5dc00aa6f9c832f36654ffdc4f02331
Benjamin Chan <chanb@ohsu.edu>
2017-11-27 13:54:41 -0800

    Increase simulation size to 500,000

--- slurm.sh
+++ slurm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=16G
 #SBATCH --partition=exacloud
-#SBATCH --time=60
+#SBATCH --time=120
 srun /usr/bin/Rscript make.r
 
